87d79445796ee864d32bf7c538e1982203264cd6
lambdasawa <lambdasawa@gmail.com>
2015-05-13 21:40:06 +0900

    4/02を解答

--- src/main/scala/com/chatwork/quiz/collection/MyList.scala
+++ src/main/scala/com/chatwork/quiz/collection/MyList.scala
@@ -56,9 +56,11 @@ case class MyCons[+A](head: A, tail: MyList[A]) extends MyList[A]
 object MyList {
 
   // Easy
-  def empty[A]: MyList[A] = ???
+  def empty[A]: MyList[A] = MyNil
 
   // Normal
-  def apply[A](as: A*): MyList[A] = ???
+  def apply[A](as: A*): MyList[A] =
+    if (as.size == 0) MyNil
+    else MyCons(as.head, apply(as.tail: _*))
 
 }
